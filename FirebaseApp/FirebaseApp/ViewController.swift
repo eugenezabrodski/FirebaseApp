de9f6059e345cb54dfac0e8bb674d4117937e4ed
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2023-01-28 15:27:52 +0300

    create

---
+++ FirebaseApp/FirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Евгений Забродский on 28.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
